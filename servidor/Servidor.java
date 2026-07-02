ef3f7683e476dbb340714b96519fb5186dbcaec4
Entony Siminhuk. <entonycco@gmail.com>
2026-07-01 21:19:56 -0300

    update final

--- servidor/Servidor.java
+++ servidor/Servidor.java
@@ -7,9 +7,9 @@ import java.rmi.registry.Registry;
 public class Servidor {
     public static void main(String[] args) {
         try {
-            ServicoJogo21 servico = new ServicoJogo21Impl(); //cria os metodos que o clinete usara
-            Registry registry = LocateRegistry.createRegistry(1099); //cria o servico de registro
-            registry.rebind("ServicoJogo21", servico); //associa um nome ao stub
+            ServicoJogo21 servico = new ServicoJogo21Impl();
+            Registry registry = LocateRegistry.createRegistry(1099);
+            registry.rebind("ServicoJogo21", servico);  
 
             System.out.println("[SERVIDOR] Servidor do Jogo 21 rodando na porta 1099. Aguardando jogadores...");
 
